b26275dca095d0823acfb7e32fd15acaf4e11c0a
JanKalin <jan.kalin@zag.si>
2016-10-18 21:52:35 +0200

    Add files via upload

---
+++ zcutils/utils.py
@@ -0,0 +1,40 @@
+###########################################################################
+# Utilities
+###########################################################################
+
+import sys
+
+###########################################################################
+# Progress indicator
+###########################################################################
+
+class Progress:
+    """Progress indicator"""
+
+    def __init__(self, fmt="{}", max_value=0):
+        """Sets max value and prints out first line. The format should be without the \\r"""
+        self.max_value = max_value
+        self.value = 0
+        self.prev = 0
+        self.fmt = "\r" + fmt
+        print self.fmt.format(self.value),
+        sys.stdout.flush()
+
+    def step(self):
+        self.value += 1
+        if not self.max_value:
+            print self.fmt.format(self.value),
+            sys.stdout.flush()
+        else:
+            self.now = int(round(100*self.value/self.max_value))
+            if not self.now == self.prev:
+                print self.fmt.format(self.now),
+                sys.stdout.flush()
+                self.prev = self.now
+            if self.value == self.max_value:
+                self.done()
+
+    def done(self):
+        print
+            
+
---
+++ zcutils/zcli.py
@@ -0,0 +1,45 @@
+###########################################################################
+# author: jank
+#
+# Simple python interface to zcash-cli
+###########################################################################
+
+import json
+import subprocess
+
+###########################################################################
+
+class zcli:
+    """Interface
+
+    This object defines the interface
+    """
+    def __init__(self, zcli):
+        """Constructor
+
+        Args:
+            zcli: path to zcash-cli
+        """
+        self.zcli = zcli
+
+
+    def _call(self, args):
+        """Calls zcash-cli and returns result
+
+        Not intended to be called directly
+        """
+        cmd = "{} {}".format(self.zcli, args)
+        buf = subprocess.check_output(cmd,
+                                      universal_newlines=True, shell=True,
+                                      stderr=subprocess.STDOUT)
+        return json.loads(buf, strict=False) if buf[0] in ['[', '{'] else buf.strip()
+
+
+    def listtransactions(self, count):
+        """Calls listtransactions to get last `count` transactions"""
+        return self._call('listtransactions "" {}'.format(count))
+                         
+    def getblock(self, blockhash):
+        """Calls listtransactions to get last `count` transactions"""
+        return self._call('getblock {}'.format(blockhash))
+                         
